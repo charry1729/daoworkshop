bcdb2ffbaba253a5176ad2c61e11720995106605
chary <balucoolguy@gmail.com>
2023-10-27 08:27:45 +0530

    first commit

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -2,23 +2,31 @@
 pragma solidity >=0.8.1;
 
 contract Migrations {
-  address public owner;
-  uint256 public last_completed_migration;
+  address public owner;              // Address of the contract owner
+
+  // Last completed migration is initially set to 2
+
+  uint256 public last_completed_migration; // Last completed migration step
 
   modifier restricted() {
     if (msg.sender == owner) _;
   }
 
-  constructor()  {
-    owner = msg.sender;
+  constructor() {
+    owner = msg.sender;// Set the contract owner as the sender of the deployment transaction
   }
 
   function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
+    // If a function call sets completed to 3, last_completed_migration is updated to 3.
+    last_completed_migration = completed; // Update the last_completed_migration variable
   }
 
   function upgrade(address new_address) public restricted {
-    Migrations upgraded = Migrations(new_address);
-    upgraded.setCompleted(last_completed_migration);
+
+    // Suppose new_address is the address of another instance of the Migrations contract.
+
+    Migrations upgraded = Migrations(new_address);  // Create a new Migrations contract at the specified address new_address
+    upgraded.setCompleted(last_completed_migration); // Set its last_completed_migration to the current value say 3
   }
 }
+
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,15 +5,13 @@ const DAO = artifacts.require("DAO")
 
 module.exports = async function (deployer, network, accounts) {
 
-// module.exports = async function (deployer, network, accounts) {
-
+  //18*6000 = 3 hrs
   await deployer.deploy(DAO,6000,6000,50);
   const dAO = await DAO.deployed();
   console.log("dAO.address : ", dAO.address);
   let config = `
 export const dAOaddress= "${dAO.address}"
 `;
-// export const documentVerificationaddress= "${documentVerification.address}"
 
   let data = JSON.stringify(config);
   fs.writeFileSync('./config.js', JSON.parse(data))
